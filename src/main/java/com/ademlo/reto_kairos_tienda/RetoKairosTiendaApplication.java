bc1dfee3892bc650bad1c50413a8077d564d5330
Javier García <javier.garcia.olmeda@gmail.com>
2026-01-20 11:32:30 +0100

    chore: creating project and add docs

---
+++ src/main/java/com/ademlo/reto_kairos_tienda/RetoKairosTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.ademlo.reto_kairos_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoKairosTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoKairosTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ademlo/reto_kairos_tienda/RetoKairosTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ademlo.reto_kairos_tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoKairosTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
